db3310fb9b546192843e8dd9c8fdbaadacbe2c8f
qiqimonkey4300 <yueqi04@gmail.com>
2020-12-08 11:51:47 -0500

    interface methods and first constructor added

---
+++ ArraySequence.java
@@ -0,0 +1,31 @@
+import java.util.NoSuchElementException;
+public class ArraySequence implements IntegerSequence{
+  private int currentIndex;
+  private int[] data;
+
+  /*Construct the sequence by copying values from the other array into the data array*/
+  public ArraySequence(int[] other){
+    data = other;
+  }
+
+  public void reset(){
+    currentIndex = 0;
+  }
+
+  public int length(){
+    return (data.length);
+  }
+
+  public boolean hasNext(){
+    return currentIndex < length();
+  }
+
+  public int next(){
+    if (!hasNext()) {
+      throw new NoSuchElementException("There is no next element.");
+    } else {
+      currentIndex++;
+      return currentIndex - 1;
+    }
+  }
+}
